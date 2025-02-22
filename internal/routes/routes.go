38ac6e083247795e6499b992a6f0dddc944c533f
Rodrigo Luz <rodrigolluzdevr@gmail.com>
2025-02-22 08:42:35 -0300

    feat: new feature - get request

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,1 +1,18 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Server() {
+	router := gin.Default()
+	router.GET("/get", getEvents)
+
+	router.Run()
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Get request success"})
+}
